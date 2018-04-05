64a4017856f2e7a92372f5cf95f103686b63e956
KARKATAHA <aaditya.dos@gmail.com>
2018-04-05 09:44:14 +0530

    testing index

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">E Shop</a>
+      <a class="navbar-brand" href="profile.php">E Shop</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
