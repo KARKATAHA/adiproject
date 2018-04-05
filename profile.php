5a75d8a878125a7d90624e46fd8f57d824fd38f9
KARKATAHA <aaditya.dos@gmail.com>
2018-04-05 20:44:29 +0530

    Updating Database

--- profile.php
+++ profile.php
@@ -47,7 +47,7 @@ if($_SESSION['is_logged_in']==0){
 	    		for($i=0;$i<$num_rows;$i++)
 	    		{
 	    			$row=mysqli_fetch_array($result);
-                    echo "<div class=\"col-md-3 col-sm-6 col-xs-12\">
+                    echo "<div class=\"col-md-3 col-sm-5 col-xs-11\">
                             <div class=\"panel panel-success\">
                                 <div class=\"panel-heading\">".$row['product_name']."</div>
                                 
