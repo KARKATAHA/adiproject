dd124ca79323b328bd0a337a3df289fe5b2e476a
KARKATAHA <aaditya.dos@gmail.com>
2018-08-19 02:09:06 +0530

    mysql

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,11 +1,10 @@
 <?php
-/*
 $con=mysqli_connect("sql9.freesqldatabase.com",'sql9252518','NE7jrGuAma');//Conection with server
 mysqli_select_db($con,'sql9252518');
-*/
-$con=mysqli_connect('mysqlinstance.czjniwbmexmc.us-east-2.rds.amazonaws.com','adityakumar','adityakumar');//Conection with server
+/*
+$con=mysqli_connect('mysqlinstance.czjniwbmexmc.us-east-2.rds.amazonaws.com','adityakumar','adityakumar');//
 mysqli_select_db($con,'adiproject');
-
+*/
 ?>
 
 
--- updateCart.php
+++ updateCart.php
@@ -1,3 +1,6 @@
 <?php
-
+/*
+$con=mysqli_connect("sql9.freesqldatabase.com",'sql9252518','NE7jrGuAma');//Conection with server
+mysqli_select_db($con,'sql9252518');
+*/
 ?>
