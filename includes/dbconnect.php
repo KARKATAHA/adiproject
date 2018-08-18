2358c3e18fa2504567cc83dacc21965d59d6816f
KARKATAHA <aaditya.dos@gmail.com>
2018-08-19 01:50:33 +0530

    mysql

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,6 +1,11 @@
 <?php
+/*
 $con=mysqli_connect("sql9.freesqldatabase.com",'sql9252518','NE7jrGuAma');//Conection with server
 mysqli_select_db($con,'sql9252518');
+*/
+$con=mysqli_connect("mysqlinstance.czjniwbmexmc.us-east-2.rds.amazonaws.com:3306",'adityakumar','adityakumar');//Conection with server
+mysqli_select_db($con,'adiproject');
+
 ?>
 
 
